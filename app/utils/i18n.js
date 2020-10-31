34912635e1a31941f106f733b4a7962896cf7b6c
Yevhen <MY_NAME@example.com>
2020-10-31 23:10:07 +0200

    removed chinese language

--- app/utils/i18n.js
+++ app/utils/i18n.js
@@ -42,42 +42,6 @@ I18n.translations = {
     cancel: 'Cancel',
     confirm: 'Confrim',
   },
-  zh: {
-    name: '数独大师',
-    continue: '继续游戏',
-    restart: '重新开始',
-    newgame: '新的游戏',
-    weekrank: '本周排行',
-    norecord: '还没有任何记录',
-    onlinerank: '在线排行',
-    rank: '您位于第 %{rank} 名',
-
-    ok: '知道了',
-    congrats: '恭喜您',
-    nosolve: '这一步之后将无解',
-    success: '成功解决本题\n用时 ',
-    fail: '您已失误超过 3 次，本次成绩无效',
-    errormove: '您已失误 %{error} 次, 超过 3 次成绩将无效',
-    newrecord: '新的解题记录\n 用时 ',
-
-    uploadrecord: '是否提交成绩？',
-    uploadmessage: '您必须先提交自己的最好成绩才能查看在线排行',
-    reject: '不提交',
-    grant: '提交',
-    loading: '正在加载中……',
-    error: '出错了',
-    uploaderror: '成绩上传失败',
-    queryerror: '数据查询失败',
-
-    share: '分享',
-    sharemessage: '数独大师 - 重拾纯粹数独的乐趣',
-    sharefailed: '分享失败',
-
-    rate: '应用评价',
-    ratemessage: '该应用为业余时间开发，您的好评是对我的最大支持',
-    cancel: '取消',
-    confirm: '确定',
-  },
 };
 
 export default I18n;
